06875636e53fac199737ef5c3568560698b54219
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-16 18:39:35 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4Tj9ymsAeQuugmLdo8r32k4_Z7bYnXWM",
+  authDomain: "whatsapp-clone-reactjs-bf6f5.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-reactjs-bf6f5.firebaseio.com",
+  projectId: "whatsapp-clone-reactjs-bf6f5",
+  storageBucket: "whatsapp-clone-reactjs-bf6f5.appspot.com",
+  messagingSenderId: "1066253558968",
+  appId: "1:1066253558968:web:ccb244e0fcd8fa4629404d",
+  measurementId: "G-BDZGVN3S35"
+};
