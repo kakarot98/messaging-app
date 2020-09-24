1d75622214dfbd326c6ce2e692fb02dc6e0c087f
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-09-24 22:09:57 +0530

    add room

--- src/components/SideChats.js
+++ src/components/SideChats.js
@@ -1,37 +1,39 @@
-import React, {useState,useEffect} from 'react'
-import './SideChats.css'
-import { Avatar } from '@material-ui/core'
+import React, { useState, useEffect } from "react";
+import "./SideChats.css";
+import { Avatar } from "@material-ui/core";
+import db from '../firebase'
 
 const SideChats = ({ id, name, addNewChat }) => {
+  const [seed, setSeed] = useState("");
 
-    const [seed, setSeed] = useState('')
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+    console.log(name);
+  }, []);
 
-    useEffect(()=>{
-        setSeed(Math.floor(Math.random() * 5000))
-        console.log(name)
-    },[])
+  const createChat = () => {
+    const roomName = prompt("Please enter the name of chat room");
 
-    const createChat = () => {
-        const roomName = prompt("Please enter the name of chat room")
-
-        if(roomName){
-            //create room
-        }
+    if (roomName) {
+      db.collection("rooms").add({
+        name: roomName,
+      });
     }
+  };
 
-    return !addNewChat ? (
-        <div className="side-chat">
-           <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-            <div className="side-chat-info">
-    <h2>{name}</h2>
-                <p>Last message...</p>
-            </div>
-        </div>
-    ) : (
-        <div onClick={createChat} className="side-chat">
-            <h2>Add New Chat</h2>
-        </div>
-    )
-}
+  return !addNewChat ? (
+    <div className="side-chat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+      <div className="side-chat-info">
+        <h2>{name}</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  ) : (
+    <div onClick={createChat} className="side-chat">
+      <h2>Add New Chat</h2>
+    </div>
+  );
+};
 
-export default SideChats
+export default SideChats;
